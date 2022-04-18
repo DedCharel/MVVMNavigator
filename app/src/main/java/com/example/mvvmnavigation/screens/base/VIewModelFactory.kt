a2acf5a01aedf04ab06ff67e3452c3b2005c5c8a
Daniil Shcherbakov <Charel@bk.ru>
2022-04-18 10:44:12 +0300

    Реализация фабрики моделей

--- app/src/main/java/com/example/mvvmnavigation/screens/base/VIewModelFactory.kt
+++ app/src/main/java/com/example/mvvmnavigation/screens/base/VIewModelFactory.kt
@@ -3,14 +3,27 @@ package com.example.mvvmnavigation.screens.base
 import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
+import com.example.mvvmnavigation.navigator.ARG_SCREEN
+import com.example.mvvmnavigation.navigator.MainNavigator
+import com.example.mvvmnavigation.navigator.Navigator
 
-class VIewModelFactory: ViewModelProvider.Factory {
+class VIewModelFactory(
+    private val screen: BaseScreen,
+    private val fragment: BaseFragment
+): ViewModelProvider.Factory {
 
     override fun <T : ViewModel?> create(modelClass: Class<T>): T {
-        TODO("Not yet implemented")
+        val hostActivity = fragment.requireActivity()
+        val application = hostActivity.application
+        val navigatorProvider = ViewModelProvider(hostActivity,ViewModelProvider.AndroidViewModelFactory(application))
+        val navigator = navigatorProvider[MainNavigator::class.java]
+
+        val constructor = modelClass.getConstructor(Navigator::class.java, screen::class.java)
+        return constructor.newInstance(navigator, screen)
     }
 }
 
 inline fun <reified VM: ViewModel> BaseFragment.screenViewModel() = viewModels<VM> {
-    VIewModelFactory()
+    val screen = requireArguments().getSerializable(ARG_SCREEN) as BaseScreen
+    VIewModelFactory(screen, this)
 }
